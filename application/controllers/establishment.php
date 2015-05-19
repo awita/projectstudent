8f7ff23b59368755290e68cc501ad5ac7dd18819
Awita <Awita.rin@gmail.com>
2015-05-19 17:09:02 +0700

    auto fill

--- application/controllers/establishment.php
+++ application/controllers/establishment.php
@@ -1,91 +1,81 @@
 <?php
-//Controller Establishment
-class establishment extends CI_Controller{
-    function __construct()
-    {
-        parent::__construct();
-           $this->load->model('establishmodel');
 
+//Controller Establishment
+class establishment extends CI_Controller {
+
+    function __construct() {
+        parent::__construct();
+        $this->load->model('establishmodel');
     }
-    
-    function home(){
+
+    function home() {
         $this->load->view('home');
         $this->load->view('template/navigation');
     }
-
 
     public function addformes() {
         $this->load->view('template/navigation_after');
         $this->load->view('establishment/establishment_formadd');
     }
-    
-    public function addestablishment()
-    {
+
+    public function addestablishment() {
         $this->load->model('establishmodel');
         $data = array(
-                'name_es'=> $this->input->post('name_es'),
-                'address_es'=> $this->input->post('address_es'),
-                'website'=> $this->input->post('website'),
-                'peple'=> $this->input->post('peple'),
-                'position'=>  $this->input->post('position'),
-                'email'=> $this->input->post('email'),
-                'tell_es'=> $this->input->post('tell_es'),
-                'website'=>  $this->input->post('website')
-                );
-        $this->db->insert('establishment',$data);    
+            'name_es' => $this->input->post('name_es'),
+            'address_es' => $this->input->post('address_es'),
+            'website' => $this->input->post('website'),
+            'peple' => $this->input->post('peple'),
+            'position' => $this->input->post('position'),
+            'email' => $this->input->post('email'),
+            'tell_es' => $this->input->post('tell_es'),
+            'website' => $this->input->post('website')
+        );
+        $this->db->insert('establishment', $data);
         $this->showestblis();
-       
-        
-    }
-    
-    public function addnew(){
-        $this->load->model('establishmodel');
-            $data['name_es']=  $this->input->post('name_es');
-            $this->db->insert('establishment',$data);
-            
     }
 
-    
-    public function showestblis()
-    {
-        
+    public function addnew() {
+        $this->load->model('establishmodel');
+        $data['name_es'] = $this->input->post('name_es');
+        $this->db->insert('establishment', $data);
+    }
+
+    public function showestblis() {
+
         $this->load->view('template/navigation_after');
         $this->load->model('studentmodel');
         $this->load->model('establishmodel');
         $this->load->session->userdata('user');
         //$user=  $this->session->userdata('user');
-        
+
         $data['establish'] = $this->studentmodel->showrelation();
         $this->load->view('establishment/showestablish', $data);
     }
-    
-    public function showdataestablish(){
+
+    public function showdataestablish() {
         $this->load->view('template/navigation');
         $this->load->model('establishmodel');
-        $data['establish'] =$this->establishmodel->showall();
-        $this->load->view('establishment/showallestablish',$data);
+        $data['establish'] = $this->establishmodel->showall();
+        $this->load->view('establishment/showallestablish', $data);
     }
 
-    public function update()
-    {
-        $id= $this->input->post('id_es');
-      
+    public function update() {
+        $id = $this->input->post('id_es');
+
         $data = array(
-               //'id_es' =>  $this->input->post('id_es'),
-               'name_es' => $this->input->post('name_es'),
-               'address_es' => $this->input->post('address_es'),
-               'position'=>  $this->input->post('position'),
-               'peple'=>  $this->input->post('peple'),
-               'tell_es'=>  $this->input->post('tell_es'),
-               'email'=>  $this->input->post('email')
-         
-            ); 
-        $this->establishmodel->update($id,$data);
-        $this->load->view('update_es',$data);
-            
+            //'id_es' =>  $this->input->post('id_es'),
+            'name_es' => $this->input->post('name_es'),
+            'address_es' => $this->input->post('address_es'),
+            'position' => $this->input->post('position'),
+            'peple' => $this->input->post('peple'),
+            'tell_es' => $this->input->post('tell_es'),
+            'email' => $this->input->post('email')
+        );
+        $this->establishmodel->update($id, $data);
+        $this->load->view('update_es', $data);
     }
-    public function deletement($id_es) 
-        {
+
+    public function deletement($id_es) {
         //public function delete($id_st) {
         if ($this->establishmodel->delete($id_es) == TRUE) {
             $data['result'] = "ลบข้อมูลเรียบร้อยแล้ว";
@@ -94,14 +84,20 @@ class establishment extends CI_Controller{
             $data['result'] = "ไม่สามารถลบข้อมูลได้ค่ะ";
         }
         redirect('/establishment/showestblis/', 'refresh');
-        }
- 
-    
-    
-   
-        
-}
+    }
 
-    
+    public function getname() {
+        $this->load->model('establishmodel');
+        $data = $this->establishmodel->showall();
+        foreach ($data as $value) {
+            echo $value['name_es'] . ',';
+        }
+    }
+    public function showdataes(){
+        $this->load->model('establishmodel');
+        $data = $this->establishmodel->showall();
+    }
+
+}
 
 ?>
--- application/views/student/add_establish.php
+++ application/views/student/add_establish.php
@@ -22,141 +22,109 @@
         <?php echo css_asset('font-awesome.min.css'); ?>
 
         <?php echo js_asset('jquery-1.10.2.js'); ?>
-
-
-
+        <style>
+            .ui-menu-item{
+                background-color: #ffffff;
+            }
+        </style>
 
     </head>
 
     <body>
 
-     
-            <div id="page-wrapper">
 
-                <div class="container-fluid">
+        <div id="page-wrapper">
 
-                    <!-- Page Heading -->
-                    <div class="row">
-                        <div class="col-md-8 col-md-offset-2">
-                            <h1 class="page-header">
-                                แสดงข้อมูลรายละเอียดนักศึกษา
-                            </h1>
-                            <ol class="breadcrumb">
-                                <li>
-                                    <i class="fa fa-dashboard"></i>  <a href="">หน้าหลัก</a>
-                                </li>
-                                <li class="active">
-                                    <i class="fa fa-table"></i> เพิ่มสถานประกอบการณ์
-                                </li>
-                            </ol>
-                        </div>
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <h1 class="page-header">
+                            แสดงการเลือกสถานประกอบการณ์
+                        </h1>
+                        <ol class="breadcrumb">
+                            <li>
+                                <i class="fa fa-dashboard"></i>  <a href="">หน้าหลัก</a>
+                            </li>
+                            <li class="active">
+                                <i class="fa fa-table"></i> เพิ่มสถานประกอบการณ์
+                            </li>
+                        </ol>
                     </div>
+                </div>
                 <?php
-                    $student = $student[0];
-                    $id_st = $student['id_st'];
-                    $name_st = $student['name_st'];
-                    $lastname_st = $student['lastname_st'];
-                    $tell_st = $student['tell_st'];
-                    $email = $student['email'];
-                    $major = $student['major'];
-                    $term = $student['term'];
-                    $year = $student['year'];
-                    $name_pr = $student['name_pr'];
-                    $relation = $student['relation'];
-                    $tell_pr = $student['tell_pr'];
-                    ?>
-                    <div class="row">
-                        <div class="col-md-8 col-md-offset-2">
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title" align='center'>เพิ่มสถานประกอบการณ์</h3>
-                                </div>
-                                <div class="panel-body"> 
-                                    <table class="table table-bordered">
-                                        <thead>
-                                            <tr>
-                                                <th width = "30%">ข้อมูล</th>
-                                                <th>รายละเอียดนักศึกษา</th>
-                                            </tr>
-                                        </thead>
-                                        <tr>
-                                            <td>ชื่อนักศึกษา</td>
-                                            <td><?php echo $name_st ?></td>
-                                        </tr>
-                                         <tr>
-                                            <td>นามสกุล</td>
-                                            <td><?php echo $lastname_st ?></td>
-                                        </tr>
-                                         <tr>
-                                            <td>รหัสนักศึกษา</td>
-                                            <td><?php echo $id_st ?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>สาขาวิชา</td>
-                                            <td><?php echo $major ?></td>
-                                        </tr>
-                                         <tr>
-                                            <td>ปีการศึกษา</td>
-                                            <td><?php echo $year ?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>เบอร์โทรศัพท์นักศึกษา</td>
-                                            <td><?php echo $tell_st ?></td>
-                                        </tr>
-                                         <tr>
-                                            <td>e-mail</td>
-                                            <td><?php echo $email ?></td>
-                                        </tr>
-                                         <tr>
-                                            <td>ชื่อผู้ปกครอง</td>
-                                            <td><?php echo $name_pr ?></td>
-                                        </tr>
-                                         <tr>
-                                            <td>ความสัมพันธ์</td>
-                                            <td><?php echo $relation ?></td>
-                                        </tr>
-                                         <tr>
-                                            <td>เบอร์โทรศัพท์</td>
-                                            <td><?php echo $tell_pr ?></td>
-                                        </tr>
-                                        <tr>
-                                            <td>กรุณาเลือกสถานประกอบการณ์</td>
-                                           
-                                           <?php   //print_r( $establish);?>
-                                        </tr>
-                                        <tr>
-                                            <td></td>
-                                            <td id='select_est'>
-                                                เลือกสถานประกอบการณ์ที่มีอยู่แล้ว 
-                                                <select name="estabishlist" id="estabishlist"  class="form-control" onchange="showmodal()">
-                                            <option>เลือกสถานประกอบการณ์</option>
-                                            <option value="-1">เพิ่มสถานประกอบการณ์ใหม่</option>
-                                            <?php foreach ($establish as $mykey => $myvalue){                                                                      
-                                            ?>
-                                            <option value="<?php echo $mykey?>"><?php echo $myvalue ?></option>
-                                            <?php }?>                                                                                      
-                                                </select>  
-                                                <p id='newest'></p>
-                                            </td>                                                                                  
-                                        </tr>
-                                    </table>
-                                    <div class="col-md-2 col-md-offset-5">
-                                        <button class="btn btn-lg btn-default" type="submit" name="submit">บันทึกข้อมูล</button>                                       
-                                    </div>                                    
-                                </div>
+                $student = $student[0];
+                $id_st = $student['id_st'];
+                $name_st = $student['name_st'];
+                $lastname_st = $student['lastname_st'];
+                $tell_st = $student['tell_st'];
+                $email = $student['email'];
+                $major = $student['major'];
+                $term = $student['term'];
+                $year = $student['year'];
+                $name_pr = $student['name_pr'];
+                $relation = $student['relation'];
+                $tell_pr = $student['tell_pr'];
+                ?>
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h3 class="panel-title" align='center'>เลือกสถานประกอบการณ์</h3>
+                            </div>
+                            <div class="panel-body"> 
+                                <table class="table ">
+
+                                    <tr>
+                                        <td width = "30%" colspan="2">กรุณาเลือกสถานประกอบการณ์</td>
+                                        <td width="60%">
+
+                                            <input class="form-control" type="text" id="name_es" name="name_es"/>
+
+
+                                        </td>  
+                                        <td> <button class="btn btn-lg btn-default" type="submit" name="submit">บันทึกข้อมูล</button> </td>
+                                    </tr>
+
+                                </table>
+                                <div class="col-md-2 col-md-offset-5">
+
+                                </div>                                    
                             </div>
                         </div>
-                    </div>                   
-                </div>
+                    </div>
+                </div>                   
             </div>
-
-        <!-- jQuery -->
-
+        </div>
         <?php echo js_asset('jquery.js'); ?>
+        <?php echo js_asset('jquery-ui.js'); ?>
+        <?php echo css_asset('jquery-ui.css'); ?>
         <!-- Bootstrap Core JavaScript -->
-
-
         <?php echo js_asset('bootstrap.min.js'); ?>
+        <!-- jQuery -->
+        <script>
+            $(function() {
+                var availableTags 
+
+                $.post("<?php echo base_url('index.php/establishment/getname');?>", function(data) {
+                    //alert("Data Loaded: " + data);
+                    var res = data.split(",");
+                    availableTags=res;
+                     $("#name_es").autocomplete({
+                    source: res
+                });
+                }); 
+            });
+            
+            <script>
+             
+            
+         
+        </script>
+        </script>
+
+
     </body>
 
 </html>
